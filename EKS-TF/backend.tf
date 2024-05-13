cb9ff1c947c0ca2acf79f1a53340bc9bcb1c6b94
vaishnav00 <60561378+vaishnav00@users.noreply.github.com>
2024-05-14 02:52:02 +0530

    Update backend.tf

--- EKS-TF/backend.tf
+++ EKS-TF/backend.tf
@@ -1,6 +1,6 @@
 terraform {
   backend "s3" {
-    bucket = "mariovs" # Replace with your actual S3 bucket name
+    bucket = "mario0" # Replace with your actual S3 bucket name
     key    = "EKS/terraform.tfstate"
     region = "ap-south-1"
   }
